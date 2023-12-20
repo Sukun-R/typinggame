9c0dec0deb60d7697cc8c435016d37c7791a92d5
Sukun-R <111569426+Sukun-R@users.noreply.github.com>
2023-12-20 18:12:52 +0900

    Update kana.js

--- kana.js
+++ kana.js
@@ -1,5 +1,11 @@
 function conversion(sentence) {
-    fetch('https://jlp.yahooapis.jp/JIMService/V2/conversion?id="dj00aiZpPWREMHU5RzZGZlVrdyZzPWNvbnN1bWVyc2VjcmV0Jng9YWE-"&jsonrpc="2.0"&method=jlp.jimservice.conversion',{
+    fetch('https://jlp.yahooapis.jp/JIMService/V2/conversion',{
+        headers:{
+            "User-Agent": "Yahoo AppID:<dj00aiZpPWREMHU5RzZGZlVrdyZzPWNvbnN1bWVyc2VjcmV0Jng9YWE->"
+        },
+        "id":5959,
+        "jsonrpc":"2.0",
+        "method":"jlp.jimservice.conversion",
         "params":{
             "q":sentence,
             "format":"hiragana",
